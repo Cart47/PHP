104333cc7fa1fc9177eef2f4d8135e23eec0978a
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-02-24 18:59:24 -0500

    test

--- cms.php
+++ cms.php
@@ -2,7 +2,6 @@
 <html>
 	<head>
 		
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/Reset.css" type="text/css">
 		<link rel="stylesheet" href="css/cms.css">
 
