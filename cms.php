b653f8b34893b33072b5e816fd017b4a775209d4
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-02-25 21:27:52 -0500

    Made changes to the CMS design

--- cms.php
+++ cms.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		
+        <link rel="stylesheet" href="css/font-awesome.min.css">
         <link rel="stylesheet" href="css/Reset.css" type="text/css">
 		<link rel="stylesheet" href="css/cms.css">
 
@@ -14,19 +15,15 @@
 
 		<div id="header-container">
 
-			<div id="date">
-				<p>Wednesday, February 18, 2015</p>
-			</div>
-
 			<ul id="cms-top-nav">
 				<li><a href=""><i class="fa fa-external-link"></i>Visit Site</a></li>
 				<li><a href=""><i class="fa fa-plus"></i>New</a></li>
 			</ul>
 
 			
-			<div id="sign-out">
-				<a href="">Sign Out</a>
-			</div>
+			
+            <a id="signout" href="">Sign Out</a>
+			
 
 			<div id="user">
 				<p>Welcome, Jane Doe</p>
@@ -34,44 +31,40 @@
 
 		</div>
 
-		<!-- Left Menu -->
-
-
-		<div id="left-menu">
-
-			<div id="logo">
-				<img src="" alt="">
-			</div>
+        <!--<div id="logo">
+            <img src="img/logo.png" alt="Chorus in the Forest Logo">
+        </div>-->
+            
+        <!-- Left Menu -->
 		
-			<div id="cms-left-nav">
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-caret-right fa-lg"></i>Home
-				</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-caret-right fa-lg"></i>About NDH
-				</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-caret-right fa-lg"></i>Patients & Visitors
-				</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-caret-right fa-lg"></i>Departments & Services
-					</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-caret-right fa-lg"></i>Join Our Team</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-caret-right fa-lg"></i>Contact</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-user fa-lg"></i>Users</a>
-				<a class="left-mnu-itm" href="">
-					<i class="fa fa-cog fa-lg"></i>Settings</a>	
-			</div>
-
-		</div>
+        <div id="cms-left-nav">
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-caret-right fa-lg"></i>
+                Home</a>
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-caret-right fa-lg"></i>
+                About NDH</a>
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-caret-right fa-lg"></i>
+                Patients & Visitors</a>
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-caret-right fa-lg"></i>
+                Departments & Services</a>
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-caret-right fa-lg"></i>
+                Join Our Team</a>
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-caret-right fa-lg"></i>
+                Contact</a>
+            <a class="left-mnu-itm" href="">
+                <i class="fa fa-user fa-lg"></i>
+                Users</a>	
+        </div>
 
 		<!-- Footer -->
 
 		<div id="cms-footer">
-			<p>&copy; 2015 Notre-Dame Hospital</p>
+			<p>&copy; 2015 Chorus in the Forest</p>
 		</div>
 		
 	</body>
