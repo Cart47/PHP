bcbe09c2d91aa1901033da8cca4dd253864d384e
Ian Gauthier <IanGauthier@me.com>
2015-03-26 19:56:53 -0400

    added the session variable to CMS admin

--- components/cms_header.php
+++ components/cms_header.php
@@ -1,9 +1,11 @@
+<?php  session_start(); ?>
 <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="../css/Reset.css">
 <link rel="stylesheet" href="../css/font-awesome-4.3.0/css/font-awesome.css">
 <link rel="stylesheet" href="../css/cms.css">
 <div id="header-container">
 
+   
     <div id="citf">Chorus in the Forest</div>
 
     <ul id="cms-top-nav">
@@ -14,7 +16,18 @@
     <a id="signout" href="">Sign Out</a>
 
     <div id="user">
-        <p>Welcome, <em>Jane Doe</em></p>
+        <p>Welcome, 
+           <em>
+               <?php
+                if(isset($_SESSION['UserFullName'])){
+                    echo $_SESSION['UserFullName'];
+                } else {
+                    echo "Unknown";   
+                }
+                
+                ?>
+           </em>
+        </p>
     </div>
 
 </div>
