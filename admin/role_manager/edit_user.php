f2e8fd4520c95684ab6e90411ebe4f8aa495f30e
Ian Gauthier <IanGauthier@me.com>
2015-04-03 13:23:27 -0400

    a

--- admin/role_manager/edit_user.php
+++ admin/role_manager/edit_user.php
@@ -7,10 +7,20 @@ include_once('../../models/role_manager/role_manager.php');
 $user = Roles::getUserRoles($_POST['edit_user']);
 ?>
 
+<!---------  BASE Role Manager ---------->
+
 <h1>Role Manager</h1>
     <br/>
     <h2>Edit Individual - <?php echo $user->getIndFName() . " " . $user->getIndLName(); ?></h2>
-
+    <br/>
+    login id
+    admin id
+    artist id
+    First Name
+    Last Name
+    email
+    
+    
 <br/>
 <a href="." class="btn xtra-pad">Cancel</a>
 
