7f5e37201c138cdc1e4f13a15c4f95980868b9c3
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-04-09 10:57:44 -0400

    had to fix the rss feed - not working on local server but works on chorusintheforest.parkthepillow.ca

---
+++ components/social.php
@@ -0,0 +1,8 @@
+<div id="icons">
+    <h2 id="connect-title">Connect with CITF</h2>
+    <img class="social-icon" src="../img/social/facebook.png" alt="Facebook"/>
+    <img class="social-icon" src="../img/social/twitter.png" alt="Twitter"/>
+    <img class="social-icon" src="../img/social/flickr.png" alt="Flickr"/>
+    <img class="social-icon" src="../img/social/youtube.png" alt="Youtube"/>
+    <a href="../news_feed/rss/" title="RSS Feed" target="_blank"><img class="social-icon" src="../img/social/rss.png" alt="RSS" /></a>
+</div>
