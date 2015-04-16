ca5de8c2f81fa37f671984932447c5964dc63b0e
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-04-16 11:05:20 -0400

    switched the database.php to controllers instead of in the header

--- components/main_header.php
+++ components/main_header.php
@@ -1,6 +1,6 @@
 <?php
 if(!isset($_SESSION)) { session_start();}
-require ('../models/database.php');  
+//require ('../models/database.php');  
 require ('../models/Registration_Login/RegisterNewUser.php');
 require ('../models/Registration_Login/UserLogin.php'); 
 
