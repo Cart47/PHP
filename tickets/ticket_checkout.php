d625d6dcefee639dcabd7c5ffa116b22094191cf
blackg33 <blackg33@gmail.com>
2015-04-20 00:27:30 -0400

    ticket update

--- tickets/ticket_checkout.php
+++ tickets/ticket_checkout.php
@@ -2,6 +2,8 @@
 
 session_start();
 include ('../components/main_header.php'); 
+require_once '../models/tickets/ticketClass.php';
+require_once '../models/tickets/ticket.php';    
 require_once '../config.php';
 
 $tick_id = $_GET['tick_id'];
@@ -9,13 +11,15 @@ $tick_type = $_GET['tick_type'];
 $tick_price = $_GET['tick_price'];
 
 //STRIPE WORKS IN CENTS - CONVERT TO DOLLARS
-$stripe_charge = $tick_price * 100;
+$stripe_charge = $tick_price * $i * 100;
 
 $_SESSION['tick_id']=$tick_id;
 $_SESSION['tick_type']=$tick_type;
 $_SESSION['tick_price']=$tick_price;
 
-$total = 78;
+$quantity = 0;
+$total = $tick_price * 4;
+
 
 ?>
 
@@ -31,9 +35,17 @@ $total = 78;
             </tr>
         </thead>
         <tbody>
-            <td><?php echo $tick_type;?><a href="ticket_type.php" class='link'> (Change)</a></td>
-            <td></td>
-            <td><select></select></td>
+            <td><?php echo $tick_type;?><a href="ticket_type.php" class='link'> Change</a></td>
+            <td>$<?php echo $tick_price; ?></td>
+            <td>
+                <select id='tick_quant'>
+                    <?php 
+                        for($i = 0; $i<21; $i++ ){
+                           echo '<option value='.$i.'>'.$i.'</option>';
+                        }
+                    ?>
+                </select>
+            </td>
             <td>$<?php echo $total ?></td>
         </tbody>
     </table>
