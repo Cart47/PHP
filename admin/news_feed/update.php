4a5d4c602db225a44ee96b8a4b40c5b5c13b0519
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-04-21 13:05:48 -0400

    added roles to the top of cms pages

--- admin/news_feed/update.php
+++ admin/news_feed/update.php
@@ -2,6 +2,12 @@
 
     require_once ('../../config.php');
     include ('../components/cms_header.php');
+    
+    //Forces a redirect through the index
+    if($_SESSION['RoleID'] != 2){
+
+      echo  '<script type="text/javascript"> window.location.href ="../../Home/Index.php"; </script>';
+    }
 
     //Forces a redirect through the index
     if(!isset($newsByID)){
