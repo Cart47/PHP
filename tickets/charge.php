b62e65c35b921fd344336f5fce025ea1f03e4f95
blackg33 <blackg33@gmail.com>
2015-04-22 18:43:27 -0400

    onchange for ticket total

--- tickets/charge.php
+++ tickets/charge.php
@@ -32,7 +32,7 @@ require '../models/database.php';?>
     
     echo
     '<div id="success">
-        <h1>SUCCESS!</h1>
+        <h1>YAY!</h1>
         <h2>Your 2015 CIFT tickets have been purchased.</h2>
     </div>';
       
