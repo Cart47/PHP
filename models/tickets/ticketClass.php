ca67231140ce7f2b07dc04e61dd98e67e6c9bac8
blackg33 <blackg33@gmail.com>
2015-04-21 20:48:38 -0400

    ticket image fix

--- models/tickets/ticketClass.php
+++ models/tickets/ticketClass.php
@@ -15,7 +15,7 @@ class ticketdb {
         
     foreach ($result as $row) {
             
-            $ticket_type = new Ticket(//$row['tick_img'],
+            $ticket_type = new Ticket($row['tick_img'],
                                       $row['tick_type'],
                                       $row['tick_price']);
             $ticket_type->setId($row['tick_id']);        
@@ -23,7 +23,29 @@ class ticketdb {
         }
         return $tickets;
     } 
-    
+    ///////////////////
+    public function getTicketsAdmin(){
+
+        $db = Database::getDB();
+        $query = 'SELECT * FROM tickets';     
+        $result = $db->query($query);
+        
+       
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+
+        $tickets = array();
+        
+    foreach ($result as $row) {
+            
+            $ticket_type = new Ticket(//$row['tick_img'],
+                                      $row['tick_type'],
+                                      $row['tick_price']);
+            $ticket_type->setId($row['tick_id']);        
+            $tickets[] = $ticket_type; 
+        }
+        return $tickets;
+    }
+    /////////////////////
     public static function insertTicket($ticket){
         
         $db = Database::getDB();
