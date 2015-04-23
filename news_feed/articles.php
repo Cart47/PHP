4550453738d272ec86a56b7afdd8f1082ed29d52
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2015-04-23 08:53:59 -0400

    quick change

--- news_feed/articles.php
+++ news_feed/articles.php
@@ -21,7 +21,7 @@ foreach ($article as $item) {
 
            //Read more button based on internal or external article
            if($item['type'] == 0) {
-               echo '<a class="more" href="http://' . $_SERVER['HTTP_HOST'] . '/news_feed/read_article.php?id=' . $item['news_id'] . '">Read More<i class="fa fa-angle-double-right fa-lg"></i></a>';
+               echo '<a class="more" href="http://' . $_SERVER['HTTP_HOST'] . 'PHP/news_feed/read_article.php?id=' . $item['news_id'] . '">Read More<i class="fa fa-angle-double-right fa-lg"></i></a>';
            } elseif($item['type'] == 1) {
                echo '<a class="more" href="' . $item['story_url'] . '" target="_blank">Read More<i class="fa fa-external-link"></i></a>';
            }
